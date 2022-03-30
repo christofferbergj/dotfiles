d8e097a44bb4e8a957947ed46bd8aa6a3d749d2c
Christoffer Berg <christofferbergj@gmail.com>
2022-03-30 12:36:28 +0200

    correct spelling

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -4,8 +4,8 @@ set -gx EDITOR nvim
 # Base colors for prompt to match iterm scheme
 set -gx fish_term24bit 0
 
-# Init starship promp
+# Init starship prompt
 starship init fish | source
 
-# Init autojump
+# Init auto-jump
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
