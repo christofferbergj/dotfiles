9b0a1b1fda0966b560ac43d26a8323a7df0f46a4
Christoffer Berg <christofferbergj@gmail.com>
2022-07-04 16:49:17 +0200

    add nvim stuff

---
+++ .config/nvim/lua/berg/colorscheme.lua
@@ -0,0 +1,7 @@
+local colorscheme = "gruvbox"
+local present, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+if not present then
+	vim.notify("colorscheme " .. colorscheme .. " not found!")
+	return
+end
