0f4f91cf51300ed85acb94162308e41f5b3af82c
Christoffer Berg <christofferbergj@gmail.com>
2022-10-20 16:55:04 +0200

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,8 +14,5 @@ set -gx fish_term24bit 0
 # Init autojump
 [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
 
-# Export N_PREFIX for tj/n install path
-set -x N_PREFIX "/opt/homebrew/bin/node"; contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin"
-
 # Init Starship
 starship init fish | source
