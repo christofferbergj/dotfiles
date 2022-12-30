880e13e2fe75800f93fcac090405f9cae1fe59df
Christoffer Berg Jensen <christofferbergj@gmail.com>
2022-12-30 16:07:27 +0100

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -14,8 +14,6 @@ return {
   -- general
   default_cursor_style = "SteadyBlock",
   color_scheme = "Gruvbox dark, medium (base16)",
-  -- color_scheme = "Rosé Pine Moon (base16)",
-  -- color_scheme = "tokyonight-storm",
   send_composed_key_when_left_alt_is_pressed = false,
   send_composed_key_when_right_alt_is_pressed = true,
 
@@ -72,42 +70,4 @@ return {
       },
     },
   },
-
-  -- Kanagawa colors
-  --colors = {
-  --	foreground = "#dcd7ba",
-  --	background = "#1f1f28",
-  --
-  --	cursor_bg = "#c8c093",
-  --	cursor_fg = "#c8c093",
-  --	cursor_border = "#c8c093",
-  --
-  --	selection_fg = "#c8c093",
-  --	selection_bg = "#2d4f67",
-  --
-  --	scrollbar_thumb = "#16161d",
-  --	split = "#16161d",
-  --
-  --	ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-  --	brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-  --	indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
-  --
-  --	tab_bar = {
-  --		-- The color of the strip that goes along the top of the window
-  --		-- (does not apply when fancy tab bar is in use)
-  --		background = "#1f1f28",
-  --		active_tab = {
-  --			bg_color = "#2A2A37",
-  --			fg_color = "#C8C093",
-  --		},
-  --		inactive_tab = {
-  --			bg_color = "#1f1f28",
-  --			fg_color = "#727169",
-  --		},
-  --		new_tab = {
-  --			bg_color = "#1f1f28",
-  --			fg_color = "#727169",
-  --		},
-  --	},
-  --},
 }
