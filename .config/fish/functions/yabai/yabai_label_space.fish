7c0ba0669b267119d04faf117f7c7ce81f747c82
Christoffer Berg Jensen <christofferbergj@gmail.com>
2023-07-07 12:54:19 +0200

    yabai overhaul

---
+++ .config/fish/functions/yabai/yabai_label_space.fish
@@ -0,0 +1,15 @@
+# Label a single space
+function yabai_label_space
+  set -l idx $argv[1]
+  set -l label $argv[2]
+
+  # Get the space on the passed index
+  set space (yabai -m query --spaces --space $idx 2>/dev/null)
+
+  if test -z "$space"
+    echo "Space $idx does not exist"
+    return
+  end
+
+  yabai -m space $idx --label $label
+end
