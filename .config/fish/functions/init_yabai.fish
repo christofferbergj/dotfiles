ceff202963a4dcb6b3ae69784c3ff8bcd49bfd1c
Christoffer Berg Jensen <christofferbergj@gmail.com>
2023-07-07 12:54:57 +0200

    add init yabai script

---
+++ .config/fish/functions/init_yabai.fish
@@ -0,0 +1,16 @@
+function init_yabai
+  # Delete all spaces
+  yabai_delete_spaces
+
+  # Setup spaces
+  yabai_setup_spaces
+
+  # Setup app rules
+  yabai_setup_app_rules
+
+  # Focus on terminal space
+  yabai -m space --focus "terminal" 2>/dev/null
+
+  echo ""
+  echo "Yabai initialisation complete ✨"
+end
