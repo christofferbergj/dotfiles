eccc4a36eb96e4b2d6f278de3d78c4a97198dd72
Christoffer Berg Jensen <christofferbergj@gmail.com>
2024-03-13 16:03:47 +0100

    add ollama commit msg script

---
+++ .config/fish/functions/ollama_commit_msg.fish
@@ -0,0 +1,28 @@
+function ollama_commit_msg -d 'Generate commit msg with Mistral'
+    # check if there are changes to commit
+    set git_diff (git diff --staged)
+    if not test -n "$git_diff"
+        echo "No changes to commit."
+        return
+    end
+
+    # check if ollama is installed
+    if not command -v ollama > /dev/null ^&1
+        echo "Ollama is not installed. Please install it before running this script. https://ollama.com/"
+        return
+    end
+
+    # generate the commit message
+    set -l commit_message (string trim -- (ollama run mistral \
+        "Generate a concise git commit message written in present tense for the following code diff with the given specifications: Commit message must be a maximum of 255 characters. One line of text and no lists of changes. Exclude anything unnecessary such as translation. Your entire response will be passed directly into git commit. $git_diff"))
+
+    echo (set_color green)"Commit message: "(set_color normal)"$commit_message"
+
+    # confirm the commit message
+    read -P "Is the commit message correct? (Y/n): " confirm
+    if test "$confirm" = "y" -o "$confirm" = ""
+        git commit -m "$commit_message"
+    else
+        echo "Commit aborted."
+    end
+end
