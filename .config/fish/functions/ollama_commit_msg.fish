a8f1b4225638a6c0f1bf5ab39cdf1c3f7a68eda0
Christoffer Berg Jensen <christofferbergj@gmail.com>
2024-03-16 16:49:13 +0100

    update

--- .config/fish/functions/ollama_commit_msg.fish
+++ .config/fish/functions/ollama_commit_msg.fish
@@ -16,12 +16,19 @@ function ollama_commit_msg -d 'Generate commit msg with Mistral'
     # generate the commit message
     set -l commit_message (string trim -- (ollama run mistral \
         "Generate a git commit message in present tense that follows these specifications:
-            1: Must be a maximum of 255 characters.
+            1: Must be a maximum of 80 characters.
             2: One line of text with no lists of changes.
-            3: Exclude anything unnecessary such as translation.
+            3: Exclude anything unnecessary such as translations and longer descriptions of the changes.
             4. Use imperative verb form e.g. 'update' instead of 'updated' and 'add' instead of 'added'.
+            5. Keep it short, concise, and to the point.
 
             Your entire response will be passed directly into git commit
+
+            Examples of good commit messages:
+            - 'Update the README with new information'
+            - 'Add new address picker to checkout'
+            - 'Add new billing queries to b2c'
+            - 'Import and implement billingQueries in the billing route'
 
             Code diff: $git_diff"))
 
