26c830777d6cb3821c0fc2f150896fc6826aeb6e
Christoffer Berg Jensen <christofferbergj@gmail.com>
2024-07-25 16:43:04 +0200

    update wezterm config object

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require("wezterm")
 local mappings = require("modules.mappings")
 
+local config = wezterm.config_builder()
+
 -- Show which key table is active in the status area
 wezterm.on("update-right-status", function(window)
 	local name = window:active_key_table()
@@ -10,66 +12,69 @@ wezterm.on("update-right-status", function(window)
 	window:set_right_status(name or "")
 end)
 
-return {
-	-- general
-	default_cursor_style = "SteadyBlock",
-	color_scheme = "Gruvbox dark, medium (base16)",
-	-- color_scheme = "Catppuccin Macchiato",
-	send_composed_key_when_left_alt_is_pressed = false,
-	send_composed_key_when_right_alt_is_pressed = true,
-	window_close_confirmation = "NeverPrompt",
+-- general
+config.default_cursor_style = "SteadyBlock"
+config.color_scheme = "Gruvbox dark, medium (base16)"
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = true
+config.window_close_confirmation = "NeverPrompt"
 
-	-- font
+-- font
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Medium" })
+config.font_size = 15
+config.line_height = 1.9
+
+-- tab bar
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_max_width = 999999
+
+-- window frame and fancy tab bar
+config.window_frame = {
+	font_size = 14,
 	font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Medium" }),
-	font_size = 15,
-	line_height = 1.9,
+}
 
-	-- tab bar
-	use_fancy_tab_bar = false,
-	tab_bar_at_bottom = true,
-	hide_tab_bar_if_only_one_tab = true,
-	tab_max_width = 999999,
+-- window
+config.window_decorations = "RESIZE"
+config.window_padding = {
+	left = 30,
+	right = 30,
+	top = 30,
+	bottom = 30,
+}
 
-	-- window
-	-- window_background_opacity = 0.93,
-	window_decorations = "RESIZE",
-	window_padding = {
-		left = 30,
-		right = 30,
-		top = 30,
-		bottom = 30,
-	},
+-- panes
+config.inactive_pane_hsb = {
+	brightness = 0.80,
+}
 
-	-- panes
-	inactive_pane_hsb = {
-		brightness = 0.80,
-	},
+-- key bindings
+config.leader = mappings.leader
+config.keys = mappings.keys
+config.key_tables = mappings.key_tables
 
-	-- key bindings
-	leader = mappings.leader,
-	keys = mappings.keys,
-	key_tables = mappings.key_tables,
+config.force_reverse_video_cursor = true
 
-	force_reverse_video_cursor = true,
-
-	colors = {
-		-- gruvbox tab_bar
-		tab_bar = {
-			-- The color of the strip that goes along the top of the window
-			-- (does not apply when fancy tab bar is in use)
-			background = "#282828",
-			active_tab = {
-				bg_color = "#32302f",
-				fg_color = "#d5c4a1",
-			},
-			inactive_tab = {
-				bg_color = "#282828",
-				fg_color = "#a89984",
-			},
-			new_tab = {
-				bg_color = "#282828",
-				fg_color = "#a89984",
-			},
+config.colors = {
+	-- Gruvbox colors (does not apply when fancy tab bar is in use)
+	tab_bar = {
+		background = "#282828",
+		active_tab = {
+			bg_color = "#32302f",
+			fg_color = "#d5c4a1",
+			intensity = "Normal",
+		},
+		inactive_tab = {
+			bg_color = "#282828",
+			fg_color = "#a89984",
+		},
+		new_tab = {
+			bg_color = "#282828",
+			fg_color = "#a89984",
 		},
 	},
 }
+
+return config
