1d1d0ae86a9eeb106f507dd6308b240433adee44
Christoffer Berg Jensen <christofferbergj@gmail.com>
2024-12-14 15:28:38 +0100

    Updated terminal and editor configurations

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -61,19 +61,37 @@ config.force_reverse_video_cursor = true
 
 config.colors = {
 	-- Gruvbox colors (does not apply when fancy tab bar is in use)
+	-- tab_bar = {
+	-- 	background = "#282828",
+	-- 	active_tab = {
+	-- 		bg_color = "#32302f",
+	-- 		fg_color = "#d5c4a1",
+	-- 		intensity = "Normal",
+	-- 	},
+	-- 	inactive_tab = {
+	-- 		bg_color = "#282828",
+	-- 		fg_color = "#a89984",
+	-- 	},
+	-- 	new_tab = {
+	-- 		bg_color = "#282828",
+	-- 		fg_color = "#a89984",
+	-- 	},
+	-- },
+
+-- Kanagawa colors
 	tab_bar = {
-		background = "#282828",
+		background = "#1f1f28",
 		active_tab = {
-			bg_color = "#32302f",
-			fg_color = "#d5c4a1",
+			bg_color = "#1f1f28",
+			fg_color = "#dcd7ba",
 			intensity = "Normal",
 		},
 		inactive_tab = {
-			bg_color = "#282828",
+			bg_color = "#1f1f28",
 			fg_color = "#a89984",
 		},
 		new_tab = {
-			bg_color = "#282828",
+			bg_color = "#1f1f28",
 			fg_color = "#a89984",
 		},
 	},
