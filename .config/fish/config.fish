b4a43b621cd1d5223cd681a7460dce3f6fd8a9fb
Christoffer Berg Jensen <christofferbergj@gmail.com>
2026-01-05 19:15:56 +0100

    Hide homebrew hints

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,3 +33,4 @@ test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 set -gx PATH /Users/christofferbergj/.local/bin $PATH
+set -gx HOMEBREW_NO_ENV_HINTS 1
