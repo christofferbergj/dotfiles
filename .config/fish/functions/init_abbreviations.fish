b6626119642a9a1aa38282cc7c3cb42e5ea5dc8b
Christoffer Berg Jensen <christofferbergj@gmail.com>
2026-02-17 18:33:17 +0100

    Fish: Update abbreviations

--- .config/fish/functions/init_abbreviations.fish
+++ .config/fish/functions/init_abbreviations.fish
@@ -6,6 +6,9 @@ function init_abbreviations -d 'Initialize fish abbreviations'
     abbr --add kp 'pnpm dlx kill-port 3000 3001 4000 6006 5173'
     abbr --add killport 'pnpm dlx kill-port 3000 3001 4000 6006 5173'
 
+    # mise
+    abbr --add u mise update-all
+
     # homebrew (package manager)
     abbr --add b brew
     abbr --add bu 'brew upgrade'
